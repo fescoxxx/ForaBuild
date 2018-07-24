3fd30d666e9c9d945b45f9e62709cb13b753d153
Роман <z.sibir@gmail.com>
2018-07-24 11:48:25 +0700

    Initial Commit

---
+++ ForaBuild/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForaBuild
+//
+//  Created by Админ on 18.07.2018.
+//  Copyright © 2018 Админ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
